f0b1df4a882970ee4448391464618bf9bf162e45
Ming Wang <MWang35@slb.com>
2023-11-01 14:23:21 +0800

    test: add normal number test

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using FizzBuzz;
 using Xunit;
 
@@ -6,12 +7,16 @@ namespace FizzBuzzTest
     public class FizzBuzzGameTest
     {
         [Fact]
-        public void Test1()
+        public void Should_return_given_number_string_when_countOff_given_normal_number()
         {
-            FizzBuzzGame game = new FizzBuzzGame();
+            // Given
+            var given_val = 2;
+            // When
+
+            var ret = FizzBuzzGame.CountOff(given_val);
 
             //then
-            Assert.NotNull(game);
+            Assert.Equal("1", ret);
         }
     }
 }
