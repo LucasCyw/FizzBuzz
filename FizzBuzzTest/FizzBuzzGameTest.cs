e590bd6a47ca8e0254f3d9a142cb955e45c5714a
Ming Wang <MWang35@slb.com>
2023-11-01 14:39:01 +0800

    test: add fizz buzz fizzbuzz test

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -16,7 +16,46 @@ namespace FizzBuzzTest
             var ret = FizzBuzzGame.CountOff(given_val);
 
             //then
-            Assert.Equal("1", ret);
+            Assert.Equal("2", ret);
+        }
+
+        [Fact]
+        public void Should_return_fizz_when_countOff_given_fizz_number()
+        {
+            // Given
+            var given_val = 3;
+            // When
+
+            var ret = FizzBuzzGame.CountOff(given_val);
+
+            //then
+            Assert.Equal("Fizz", ret);
+        }
+
+        [Fact]
+        public void Should_return_buzz_when_countOff_given_buzz_number()
+        {
+            // Given
+            var given_val = 5;
+            // When
+
+            var ret = FizzBuzzGame.CountOff(given_val);
+
+            //then
+            Assert.Equal("Buzz", ret);
+        }
+
+        [Fact]
+        public void Should_return_FizzBuzz_when_countOff_given_FizzBuzz_number()
+        {
+            // Given
+            var given_val = 15;
+            // When
+
+            var ret = FizzBuzzGame.CountOff(given_val);
+
+            //then
+            Assert.Equal("FizzBuzz", ret);
         }
     }
 }
