1384a36319faf0f6ec74ca81060769cc9880b434
Ming Wang <MWang35@slb.com>
2023-11-01 14:34:01 +0800

    feat: add countoff function

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -1,6 +1,27 @@
-﻿namespace FizzBuzz
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
 {
     public class FizzBuzzGame
     {
+        public static string CountOff(int given_val)
+        {
+            if (given_val % 3 == 0)
+            {
+                if (given_val % 5 == 0)
+                {
+                    return "FizzBuzz";
+                }
+
+                return "Fizz";
+            }
+            else if (given_val % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return given_val.ToString();
+        }
     }
 }
